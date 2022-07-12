3ccc756d2cbfe2d09d3e4534eb761a07d2c04980
MaryAtame <masha.atamasova@ya.ru>
2022-07-12 16:11:36 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,111 @@
+$(function() {
+    
+    /* fixed header */
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
+    
+     checkscroll(scrollPos, introH);
+    
+    $(window).on("scroll resize", function(){
+        introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+    
+        checkscroll(scrollPos, introH);
+    
+    });
+    
+    function checkscroll(scrollPos, introH) {
+         if( scrollPos > introH ) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+    
+    /* smooth scroll */
+    
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+        
+        let elementId = $(this).data('scroll');
+        let elementOffset = $(elementId).offset().top;
+        
+        nav.removeClass("show");
+        
+        
+        console.log(elementOffset);
+        
+        $("html, body").animate({
+            scrollTop: elementOffset - 70
+        }, 700);
+    });
+    
+    
+    /* Nav Toggle */
+    
+    navToggle.on("click", function(event) {
+        event.preventDefault();
+        
+        nav.toggleClass("show");
+    });
+    
+    
+    
+    /* reviews: https://kenwheeler.github.io/slick/ */
+    
+    let slider = $("#reviewsSlider");
+    
+    slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        dots: true
+        
+    });
+    
+    
+    
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
